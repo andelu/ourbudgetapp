963f92397e7e23e48ab0ed3ea7694ddad4dc9704
andelu <andres.china@yahoo.com>
2019-01-08 19:48:22 +0800

    update

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 //Start of the file
+class Expenses{
+    constructor(){
+    this.exList=[]
+    }
+    addexpense(name,cost)
+    let newExpense{
+        name: name,
+        cost: cost
+    }
+    this.expList.push(newExpense)
+}
+class Incomes{
+    constructor(){
+    this.inList=[]
+    }
+    addincome(name,cost)
+    let newExpense{
+        name: name,
+        cost: cost
+    }
+    this.expList.push(newExpense)
+}
+
+let expList=[]
+let incList=[]
 
 // this is my line
