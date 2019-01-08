369c3b64e3562792ba56b82a6c4ec6610bf8a7f7
andelu <andres.china@yahoo.com>
2019-01-08 20:10:15 +0800

    update2

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 //Start of the file
-class Expenses{
-    constructor(){
-    this.exList=[]
-    }
-    addexpense(name,cost)
-    let newExpense{
-        name: name,
-        cost: cost
-    }
-    this.expList.push(newExpense)
-}
-class Incomes{
-    constructor(){
-    this.inList=[]
-    }
-    addincome(name,cost)
-    let newExpense{
-        name: name,
-        cost: cost
-    }
-    this.expList.push(newExpense)
-}
-
 let expList=[]
 let incList=[]
+let arrayT=0
+let totalMoney=0
+let arrayTotal=(array)=>{
+    //array.value should be changed to number type ahead
+    for(i=0;i>array.length;i++){
+        arrayT=arrayT+array.value
+    }
+}
+let moneyLeft=()=>{
+    totalMoney=arrayTotal(expList)-arrayTotal(expList)
+}
+
 
 // this is my line
