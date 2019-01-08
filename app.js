0242a9eb93b6dbedec0d95db04bdbee125e41612
Alex <alexolthoff@yahoo.com>
2019-01-08 04:37:43 -0800

    GOOD STUFF

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let moneyLeft=()=>{
     totalMoney=arrayTotal(expList)-arrayTotal(expList)
 }
 
-
+// Data Storage
 const expList=[] 
 const incList=[]
 
@@ -26,4 +26,18 @@ const addtoArray = (number,item,choice) => {
         expList.push(record)
     }
 }
-// this is my line
+// Getting from the DOM
+const getnumber = () => {
+return document.getElementById("value").value }
+const gettext = () => {
+    return document.getElementById("item").value
+}
+const getChoice = () => {
+    return document.getElementById('choice').value
+}
+const getexpensetotal = () => {
+    return document.getElementById('expensetotal').value
+}
+const getIncometotal = () => {
+    return document.getElementById('incomeTotal').value
+}
